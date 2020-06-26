56a3c287e0adebe809080658e83ef155032ce148
Jim Mainprice <mainprice@gmail.com>
2020-06-26 14:20:28 +0200

    Add common immport

--- learch2/learch.py
+++ learch2/learch.py
@@ -16,20 +16,22 @@
 # PERFORMANCE OF THIS SOFTWARE.
 #
 #                                        Jim Mainprice on Sunday June 13 2018
-
-from src.pyrieef.pyrieef.learning.inverse_optimal_control import *
-from src.pyrieef.pyrieef.geometry.workspace import *
-from src.pyrieef.pyrieef.geometry.interpolation import *
-from src.pyrieef.pyrieef.graph.shortest_path import *
-import src.pyrieef.pyrieef.rendering.workspace_renderer as render
+import common_import
+from pyrieef.learning.inverse_optimal_control import *
+from pyrieef.geometry.workspace import *
+from pyrieef.geometry.interpolation import *
+from pyrieef.graph.shortest_path import *
+import pyrieef.rendering.workspace_renderer as render
 from scipy.interpolate import Rbf
 import numpy as np
 import time
 import matplotlib.pyplot as plt
 
+
 class Learch2D(Learch):
 
-    def __init__(self, nb_points, centers, sigma, paths, starts, targets, pixel_map):
+    def __init__(self,
+                 nb_points, centers, sigma, paths, starts, targets, pixel_map):
         Learch.__init__(self, len(paths))
 
         self._loss_scalar = .8
@@ -45,14 +47,16 @@ class Learch2D(Learch):
         self.w = np.ones(len(centers))
         self.costmap = np.zeros((nb_points, nb_points))
         self.loss_map = np.zeros((len(paths), nb_points, nb_points))
-        self.d = np.empty((3,0))
+        self.d = np.empty((3, 0))
         self.pixel_map = pixel_map
         self.initialize_mydata()
 
     def initialize_mydata(self):
         for i, t in enumerate(self.sample_trajectories):
-            self.loss_map[i] = scaled_hamming_loss_map(t, self.nb_points, self._loss_scalar, self._loss_stddev)
-        self.costmap = get_costmap(self.nb_points, self.centers, self.sigma, self.w)
+            self.loss_map[i] = scaled_hamming_loss_map(
+                t, self.nb_points, self._loss_scalar, self._loss_stddev)
+        self.costmap = get_costmap(
+            self.nb_points, self.centers, self.sigma, self.w)
 
     def planning(self):
         for i, trajectory in enumerate(self.sample_trajectories):
@@ -65,7 +69,9 @@ class Learch2D(Learch):
             t = self.pixel_map.world_to_grid(self.sample_targets[i])
 
             try:
-                optimal_path = converter.dijkstra_on_map(np.exp(self.costmap - self.loss_map[i]), s[0], s[1], t[0], t[1])
+                optimal_path = converter.dijkstra_on_map(
+                    np.exp(self.costmap - self.loss_map[i]),
+                    s[0], s[1], t[0], t[1])
             except Exception as e:
                 print("Exception")
                 continue
@@ -73,7 +79,7 @@ class Learch2D(Learch):
 
             x1 = np.asarray(np.transpose(optimal_path)[:][0])
             x2 = np.asarray(np.transpose(optimal_path)[:][1])
-            d = np.vstack((x1, x2, self.costmap[x1,x2] + g[x1, x2]))
+            d = np.vstack((x1, x2, self.costmap[x1, x2] + g[x1, x2]))
             self.d = np.hstack((self.d, d))
             x1 = np.asarray(np.transpose(trajectory)[:][0])
             x2 = np.asarray(np.transpose(trajectory)[:][1])
@@ -88,7 +94,8 @@ class Learch2D(Learch):
 
         Phi = self.phi[:, x1, x2].T
         self.w = linear_regression(Phi, C, self.w, 6, 0)
-        self.costmap = get_costmap(self.nb_points, self.centers, self.sigma, self.w)
+        self.costmap = get_costmap(
+            self.nb_points, self.centers, self.sigma, self.w)
 
     def one_step(self):
         self.planning()
@@ -108,8 +115,8 @@ class Learch2D(Learch):
             self.one_step()
             print("w: ", self.w)
             maps.append(self.costmap)
-            e_p = (np.absolute(self.costmap-old_c)).sum()
-            e_o = (np.absolute(self.costmap-original_costmap)).sum()
+            e_p = (np.absolute(self.costmap - old_c)).sum()
+            e_o = (np.absolute(self.costmap - original_costmap)).sum()
             print("error with previous: ", e_p)
             print("error with original: ", e_o)
             old_c = copy.deepcopy(self.costmap)
@@ -117,35 +124,38 @@ class Learch2D(Learch):
 
 
 def scaled_hamming_loss_map(trajectory, nb_points,
-                 goodness_scalar,
-                 goodness_stddev):
+                            goodness_scalar,
+                            goodness_stddev):
     occpancy_map = np.zeros((nb_points, nb_points))
     for x_1, x_2 in trajectory:
         occpancy_map[x_1][x_2] = 1
     goodness = goodness_scalar * np.exp(-0.5 * (
         edt(occpancy_map) / goodness_stddev)**2)
-    return 1-goodness
+    return 1 - goodness
 
 
 def hamming_loss_map(trajectory, nb_points,
-                 goodness_scalar,
-                 goodness_stddev):
+                     goodness_scalar,
+                     goodness_stddev):
     occpancy_map = np.ones((nb_points, nb_points))
     for x_1, x_2 in trajectory:
         occpancy_map[x_1][x_2] = 0
     return occpancy_map
 
+
 def get_gradient(costmap):
     g = np.ones(costmap.shape)
     #g = np.gradient(costmap)
     # gradient fixed for now
-    return  g * 0.5 #g[o]
+    return g * 0.5  # g[o]
+
 
 def show_map(costmap, workspace):
     if show_result:
         viewer = render.WorkspaceDrawer(workspace, wait_for_keyboard=True)
         viewer.draw_ws_img(costmap, interpolate="none")
         viewer.show_once()
+
 
 def show_samples(costmap, starts, targets, paths, workspace):
     if show_result:
@@ -161,15 +171,18 @@ def show_samples(costmap, starts, targets, paths, workspace):
             viewer.draw_ws_point(t_w, color=c)
         viewer.show_once()
 
+
 def show_multiple_maps(costmaps, workspace):
     if show_result:
         r = math.ceil(math.sqrt(len(costmaps)))
         c = math.ceil(len(costmaps) / r)
-        viewer = render.WorkspaceDrawer(workspace, wait_for_keyboard=True, rows=r, cols=c)
+        viewer = render.WorkspaceDrawer(
+            workspace, wait_for_keyboard=True, rows=r, cols=c)
         for i, costmap in enumerate(costmaps):
             viewer.set_drawing_axis(i)
             viewer.draw_ws_img(costmap, interpolate="none")
         viewer.show_once()
+
 
 def get_rbf(nb_points, center, sigma):
     X, Y = workspace.box.meshgrid(nb_points)
@@ -177,6 +190,7 @@ def get_rbf(nb_points, center, sigma):
     map = rbf(X, Y)
 
     return map
+
 
 def get_phi(nb_points, centers, sigma):
     rbfs = []
@@ -186,10 +200,11 @@ def get_phi(nb_points, centers, sigma):
 
     return phi
 
+
 def get_costmap(nb_points, centers, sigma, w):
     costmap = np.tensordot(w, get_phi(nb_points, centers, sigma), axes=1)
-
     return costmap
+
 
 cmap = plt.get_cmap('viridis')
 
@@ -204,7 +219,7 @@ workspace = Workspace()
 pixel_map = workspace.pixel_map(nb_points)
 np.random.seed(1)
 
-#create costmap with rbfs
+# create costmap with rbfs
 w = np.random.random(nb_rbfs**2)
 centers = workspace.box.meshgrid_points(nb_rbfs)
 phi = get_phi(nb_points, centers, sigma)
@@ -212,6 +227,7 @@ original_costmap = get_costmap(nb_points, centers, sigma, w)
 maps = [original_costmap]
 
 #show_map(original_costmap, workspace)
+
 
 # Plan path
 converter = CostmapToSparseGraph(original_costmap, average_cost)
@@ -229,7 +245,8 @@ for i in range(nb_samples):
     try:
         print("planning...")
         time_0 = time.time()
-        path = converter.dijkstra_on_map(np.exp(original_costmap), s[0], s[1], t[0], t[1])
+        path = converter.dijkstra_on_map(
+            np.exp(original_costmap), s[0], s[1], t[0], t[1])
     except Exception as e:
         print("Exception")
 
@@ -240,6 +257,6 @@ for i in range(nb_samples):
 
 show_samples(original_costmap, starts, targets, paths, workspace)
 
-#learn costs
+# learn costs
 l = Learch2D(nb_points, centers, sigma, paths, starts, targets, pixel_map)
 l.n_step(20)
