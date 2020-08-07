correct loss calculation in hyperparametersearch

analysis/hyperparametersearch.py
@@ -122,13 +122,13 @@ for j in range(nb_runs):
             # Add regularization factor to loss
             if learning == 'learch':
                 loss[j, int(i / parameter_step) - param_lower_bound] = \
-                    (loss[j, int(i / parameter_step) - param_lower_bound] / i) + \
+                    (loss[j, int(i / parameter_step) - param_lower_bound] / nb_samples) + \
                     (l._l2_regularizer + l._proximal_regularizer) \
-                    * np.linalg.norm(w_t)
+                    * np.linalg.norm(w_t[-1])
             elif learning == 'maxEnt':
                 loss[j, int(i / parameter_step) - param_lower_bound] = \
-                    (loss[j, int(i / parameter_step) - param_lower_bound] / i) + \
-                    np.linalg.norm(w_t)
+                    (loss[j, int(i / parameter_step) - param_lower_bound] / nb_samples) + \
+                    np.linalg.norm(w_t[-1])
         except KeyboardInterrupt:
             break
         except Exception:

my_utils/output.py
@@ -272,8 +272,8 @@ def plot_from_file(show_image, directory, directoryToSave):
         ax.set_xlabel('proximal regularization')
         ax.set_ylabel('l2 regularization')
     elif param == 'loss':
-        ax.set_xlabel('loss scalar')
-        ax.set_ylabel('loss stddev')
+        ax.set_xlabel('loss stddev')
+        ax.set_ylabel('loss scalar')
     x_1, x_2 = np.meshgrid(x1, x2)
     norm = plt.Normalize(y.min(), y.max())
     colors = cm.viridis(norm(y))