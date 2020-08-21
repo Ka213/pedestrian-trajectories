a5d9dd579a754746dae5db1c46ca51d66a770277
KatharinaH <katharina.hengel@gmail.com>
2020-08-21 09:38:08 +0200

    add code to compare the results of LEARCH and maxEnt

---
+++ analysis/compare_learning_methods.py
@@ -0,0 +1,13 @@
+from common_import import *
+
+from my_utils.output_analysis import *
+
+path = home + '/../prediction/'
+
+directory_learch = path + "/learch_1runs_1-100samples_100predictions" \
+                   + "/results.npz"
+directory_maxEnt = path + "/maxEnt_1runs_1-100samples_100predictions" \
+                   + "/results.npz"
+directory = path + "/comparison_100_samples_100predictions.png"
+
+compare_learning_methods(directory_learch, directory_maxEnt, directory)
