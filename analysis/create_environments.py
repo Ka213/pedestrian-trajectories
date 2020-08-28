530ece2f835028686b4a8431fa08abf65c0443df
KatharinaH <katharina.hengel@gmail.com>
2020-08-28 09:14:57 +0200

    create sample environments

---
+++ analysis/create_environments.py
@@ -0,0 +1,26 @@
+import common_import
+
+from my_utils.environment import *
+from my_utils.output_costmap import *
+from my_utils.my_utils import *
+
+show_result = 'SHOW'
+with_trajectories = True
+average_cost = False
+nb_points = 40
+nb_rbfs = 5
+sigma = 0.1
+nb_samples = 500
+nb_environments = 5
+
+workspace = Workspace()
+
+# Create random environments
+for i in range(nb_environments):
+    print(i)
+    np.random.seed(i)
+    w, costmap, starts, targets, paths = \
+        create_random_environment(nb_points, nb_rbfs, sigma, nb_samples,
+                                  workspace)
+    save_environment('environment{}_{}samples'.format(i, nb_samples), nb_points, nb_rbfs, sigma, nb_samples, w,
+                     costmap, starts, targets, np.asarray(paths))
