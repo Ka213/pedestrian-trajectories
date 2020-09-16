956a320af48e8fdb10985edab0ae1bc14b25da1c
KatharinaH <katharina.hengel@gmail.com>
2020-09-16 09:25:11 +0200

    maxEnt on multiple environments

--- my_learning/max_ent.py
+++ my_learning/max_ent.py
@@ -7,99 +7,139 @@ from my_utils.environment import *
 class MaxEnt():
     """ Implements the Maximum Entropy approach for a 2D squared map """
 
-    def __init__(self, nb_points, centers, sigma,
-                 paths, starts, targets, workspace):
-
+    def __init__(self, nb_points, nb_rbfs, sigma, workspace):
         self.workspace = workspace
-
-        # Parameters to compute the step size
-        self._learning_rate = 0.14
-        self._stepsize_scalar = 20
-
-        self._N = 191
-
         # Parameters to compute the cost map from RBFs
         self.nb_points = nb_points
-        self.centers = centers
         self.sigma = sigma
-        self.phi = get_phi(nb_points, centers, sigma, workspace)
+        self.nb_rbfs = nb_rbfs
+        # Parameters to compute the step size
+        self._learning_rate = 0.114
+        self._stepsize_scalar = 20
 
-        # Examples
-        self.sample_trajectories = paths
-        self.sample_starts = starts
-        self.sample_targets = targets
+        self.convergence = 0.01
 
-        self.w = np.zeros(len(centers))
-        self.costmap = get_costmap(
-            self.nb_points, self.centers, self.sigma, self.w, self.workspace)
-        self.transition_probability = \
-            get_transition_probabilities(self.costmap, self.nb_points)
+        self.w = np.zeros(nb_rbfs ** 2)
+        self.instances = []
 
-        # Data structures to save the progress of MaxEnt in each iteration
-        self.maps = [self.costmap]
-        self.weights = [self.w]
+    def add_environment(self, centers, paths, starts, targets):
+        """ Add an new environment to the maxEnt computation """
+        phi = get_phi(self.nb_points, centers, self.sigma, self.workspace)
+        M = self.MaxEnt_instance(phi, paths, starts, targets, self.workspace)
+        self.instances.append(M)
 
-    def one_step(self, t):
-        """ Compute one gradient descent step """
-        time_0 = time.time()
-        print("step :", t)
-        self.w = self.w + get_stepsize(t, self._learning_rate,
-                                       self._stepsize_scalar) \
-                 * self.get_gradient()
-        self.costmap = get_costmap(self.nb_points, self.centers, self.sigma,
-                                   self.w, self.workspace)
-        print("took t : {} sec.".format(time.time() - time_0))
-        self.maps.append(self.costmap)
-        self.weights.append(self.w)
-        return self.maps, self.weights
-
-    def n_step(self, n):
-        """ Compute n gradient descent step """
+    def n_steps(self, n):
+        """ Do n steps of the maxEnt algorithm over multiple environments """
+        step = 0
         for i in range(n):
-            self.one_step(i)
-        return self.maps, self.weights
+            print("step :", step)
+            g = np.zeros(self.nb_rbfs ** 2)
+            for j, i in enumerate(self.instances):
+                i.w = self.w
+                i.costmap = np.tensordot(self.w, i.phi, axes=1)
+                gradient = i.get_gradient()
+                g += gradient
+            self.w = self.w + get_stepsize(step, self._learning_rate,
+                                           self._stepsize_scalar) * \
+                     (g / len(self.instances))
+            # print("w ", self.w , self.w.sum())
+            print("step size: ", get_stepsize(step, self._learning_rate,
+                                              self._stepsize_scalar))
+            step += 1
+        costmaps = []
+        optimal_paths = []
+        for _, i in enumerate(self.instances):
+            costmap = np.tensordot(self.w, i.phi, axes=1)
+            costmaps.append(costmap)
+            _, _, paths = plan_paths(len(i.sample_trajectories), costmap,
+                                     self.workspace, starts=i.sample_starts,
+                                     targets=i.sample_targets)
+            optimal_paths.append(paths)
+        return costmaps, optimal_paths, self.w, step
 
     def solve(self):
-        """ Compute gradient descent until convergence """
+        """ Compute the maxEnt over multiple environments
+            until the weights converge
+        """
+        step = 0
         w_old = copy.deepcopy(self.w)
         e = 10
-        i = 0
-        while e > 0.004:
-            self.one_step(i)
-            # print("w: ", self.w)
-            # e = (np.absolute(self.w - w_old)).sum()
-            e = (np.amax(self.w - w_old)).sum()
+        while e > self.convergence:
+            print("step :", step)
+            g = np.zeros(self.nb_rbfs ** 2)
+            for j, i in enumerate(self.instances):
+                i.w = self.w
+                i.costmap = np.tensordot(self.w, i.phi, axes=1)
+                gradient = i.get_gradient()
+                g += gradient
+            self.w = self.w + get_stepsize(step, self._learning_rate,
+                                           self._stepsize_scalar) * \
+                     (g / len(self.instances))
+            # print("w ", self.w , self.w.sum())
+            print("step size: ", get_stepsize(step, self._learning_rate,
+                                              self._stepsize_scalar))
+            e = np.amax(self.w - w_old).sum()
             print("convergence: ", e)
             w_old = copy.deepcopy(self.w)
-            i += 1
-        return self.maps, self.weights
+            step += 1
+        costmaps = []
+        optimal_paths = []
+        for _, i in enumerate(self.instances):
+            costmap = np.tensordot(self.w, i.phi, axes=1)
+            costmaps.append(costmap)
+            _, _, paths = plan_paths(len(i.sample_trajectories), costmap,
+                                     self.workspace, starts=i.sample_starts,
+                                     targets=i.sample_targets)
+            optimal_paths.append(paths)
+        return costmaps, optimal_paths, self.w, step
 
-    def get_gradient(self):
-        """ Compute the gradient of the maximum entropy objective """
-        Phi = get_phi(self.nb_points, self.centers, self.sigma, self.workspace)
-        # Calculate expected empirical feature counts
-        f_empirical = get_empirical_feature_count \
-            (self.sample_trajectories, Phi)
-        # Calculate the learners expected feature count
-        try:
-            D = get_expected_edge_frequency(self.transition_probability,
-                                            self.costmap, self._N,
-                                            self.nb_points, self.sample_targets,
-                                            self.sample_trajectories,
-                                            self.workspace)
-        except Exception:
-            raise
-        f_expected = np.tensordot(Phi, D)
-        f = f_empirical - f_expected
-        f = - f - np.min(- f)
-        return f
+    class MaxEnt_instance():
+        """ Implements the maxEnt algorithm for one environment """
+
+        def __init__(self, phi, paths, starts, targets, workspace):
+            self.workspace = workspace
+
+            self._N = 185
+
+            # Examples
+            self.sample_trajectories = paths
+            self.sample_starts = starts
+            self.sample_targets = targets
+
+            self.phi = phi
+            self.w = np.zeros(phi.shape[0])
+            self.costmap = np.tensordot(self.w, self.phi, axes=1)
+            self.transition_probability = \
+                get_transition_probabilities(self.costmap, phi.shape[1])
+
+        def get_gradient(self):
+            """ Compute the gradient of the maximum entropy objective """
+            # Calculate expected empirical feature counts
+            f_empirical = get_empirical_feature_count \
+                (self.sample_trajectories, self.phi)
+            # Calculate the learners expected feature count
+            try:
+                D = get_expected_edge_frequency(self.transition_probability,
+                                                self.costmap, self._N,
+                                                self.phi.shape[1],
+                                                self.sample_targets,
+                                                self.sample_trajectories,
+                                                self.workspace)
+            except Exception:
+                raise
+            f_expected = np.tensordot(self.phi, D)
+            f = f_empirical - f_expected
+            f = - f - np.min(- f)
+            return f
 
 
-def get_maxEnt_loss(learned_map, demonstrations, nb_samples, w):
+def get_maxEnt_loss(learned_maps, demonstrations, nb_samples, w=None):
+    """ Returns the maxEnt loss with or without regularization factor """
     loss = 0
-    for d in zip(demonstrations):
-        loss += \
-            np.sum(learned_map[np.asarray(d).T[:][0],
-                               np.asarray(d).T[:][1]])
-    loss = (loss / nb_samples) + np.linalg.norm(w)
+    for map, demo in zip(learned_maps, demonstrations):
+        for _, d in enumerate(demo):
+            loss += np.sum(map[np.asarray(d)[:, 0], np.asarray(d)[:, 1]])
+    loss = (loss / nb_samples)
+    if w is not None:
+        loss += np.linalg.norm(w)
     return loss
