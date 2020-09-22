4409f8969bc7ef6307a28c54e63853ba85f95436
KatharinaH <katharina.hengel@gmail.com>
2020-09-22 21:18:31 +0200

    update tests to multiple environments

--- tests/test_learch.py
+++ tests/test_learch.py
@@ -8,27 +8,22 @@ def test_supervised_learning():
     nb_points = 40
     nb_rbfs = 5
     sigma = 0.1
-    nb_samples = 10
 
     workspace = Workspace()
-    np.random.seed(1)
+
+    l = Learch2D(nb_points, nb_rbfs, sigma, workspace)
+    np.random.seed(0)
     # Create random costmap
     w, original_costmap, starts, targets, paths, centers = \
-        create_rand_env(nb_points, nb_rbfs, sigma, nb_samples,
-                        workspace)
-
-    l = Learch2D(nb_points, centers, sigma, paths, starts, targets, workspace)
-    l.exponentiated_gd = True
-    if l.exponentiated_gd:
-        t = 1
-    else:
-        t = 0.001
-    l.D = np.zeros((3, nb_points ** 2))
+        load_environment("environment_sample_centers0")  # \
+    #    create_random_environment(nb_points, nb_rbfs, sigma, nb_samples, workspace)
+    # Learn costmap
+    l.add_environment(centers, paths, starts, targets)
 
     # adjust parameters
     l._learning_rate = 1
     l._stepsize_scalar = 1
-    l._l2_regularizer = 1
+    l._l2_regularizer = 1e-10
     l._proximal_regularizer = 0
 
     # iterate LEARCH until weights are converged
@@ -36,25 +31,35 @@ def test_supervised_learning():
     e = 10
     i = 0
 
-    while e > t:
+    while e > 1e-10:
+        print("step: ", i)
+        l.instances[0].update(l.w)
         # construct D from all states of the original analysis
         x, y = np.meshgrid(range(nb_points), range(nb_points))
         x_1 = np.reshape(y, nb_points ** 2)
         x_2 = np.reshape(x, nb_points ** 2)
-        y = np.reshape(original_costmap, nb_points ** 2)
-        l.D = np.vstack((x_1, x_2, y))
-        l.supervised_learning(i)
-        maps = l.maps
-        e = np.amax(np.absolute(l.weights[-1] - w_old))
+        y = np.reshape(original_costmap -
+                       l.instances[0].learned_maps[-1], nb_points ** 2)
+        D = np.vstack((x_1, x_2, y))
+        w_t = l.instances[0].supervised_learning(D)
+        l.w = l.w * np.exp(get_stepsize(i, l._learning_rate,
+                                        l._stepsize_scalar) * w_t)
+        print("step size:", get_stepsize(i, l._learning_rate,
+                                         l._stepsize_scalar))
+        e = np.amax(np.absolute(l.w - w_old))
         print("convergence: ", e)
-        w_old = copy.deepcopy(l.weights[-1])
+        w_old = copy.deepcopy(l.w)
         i += 1
-    print((np.absolute(maps[-1] - original_costmap)).sum())
-    assert (np.absolute(maps[-1] - original_costmap)).sum() < \
-           nb_points ** 2 * 1.5
-    show_multiple(maps[::40], [original_costmap], workspace, show_result,
-                  directory=home +
-                            '/../results/figures/test_supervised_learning.png')
+        # show_multiple([l.instances[0].costmap], [original_costmap], workspace,
+        #              show_result, weights=[w_t], centers=centers)
+
+    costmap = np.tensordot(np.log(l.w), l.instances[0].phi, axes=1)
+    costs = np.sum(np.abs(costmap - original_costmap))
+    print("costs: ", costs)
+    assert np.average(costs) < 1e-4
+    show_multiple(l.instances[0].learned_maps[::200], [original_costmap],
+                  workspace, show_result, directory=home +
+                                                    '/../results/figures/test_supervised_learning.png')
 
 def test_D():
     nb_points = 40
@@ -63,62 +68,31 @@ def test_D():
     nb_samples = 5
 
     workspace = Workspace()
-    np.random.seed(1)
-    w, original_costmap, starts, targets, paths, centers = \
-        create_rand_env(nb_points, nb_rbfs, sigma, nb_samples, workspace)
 
-    l = Learch2D(nb_points, centers, sigma, paths, starts, targets, workspace)
-    l.planning()
+    l = Learch2D(nb_points, nb_rbfs, sigma, workspace)
+
+    np.random.seed(0)
+    # Create random costmap
+    w, original_costmap, starts, targets, paths, centers = \
+        load_environment("environment_sample_centers0")  # \
+    #    create_random_environment(nb_points, nb_rbfs, sigma, nb_samples, workspace)
+    starts = starts[:nb_samples]
+    targets = targets[:nb_samples]
+    paths = paths[:nb_samples]
+    # Learn costmap
+    l.add_environment(centers, paths, starts, targets)
+
+    D, _ = l.instances[0].planning()
     len_paths = [len(p) for p in paths]
-    len_optimal_paths = [len(op) for op in l.optimal_paths[-1]]
-    assert l.D.shape == (3, np.sum(len_paths) + np.sum(len_optimal_paths))
-    print(l.D)
-    show(l.costmap, workspace, show_result, starts=starts, targets=targets,
-         paths=paths, optimal_paths=l.optimal_paths[-1], d=l.D,
+    len_optimal_paths = [len(op) for op in l.instances[0].optimal_paths[-1]]
+    assert D.shape == (3, np.sum(len_paths) + np.sum(len_optimal_paths))
+    print(D)
+    show(l.instances[0].costmap, workspace, show_result, starts=starts,
+         targets=targets, paths=paths,
+         optimal_paths=l.instances[0].optimal_paths[-1], d=D,
          directory=home + '/../results/figures/map_with_D.png')
 
 
-def test_learch():
-    nb_points = 40
-    nb_rbfs = 5
-    sigma = 0.1
-    nb_samples = 5
-
-    workspace = Workspace()
-    np.random.seed(1)
-    # Create random costmap
-    w, original_costmap, starts, targets, paths, centers = \
-        create_rand_env(nb_points, nb_rbfs, sigma, nb_samples, workspace)
-
-    l = Learch2D(nb_points, centers, sigma, paths, starts, targets, workspace)
-
-    # iterate LEARCH until weights are converged
-    w_old = copy.deepcopy(l.w)
-    e = 10
-    cost = 0
-    cost_old = 10000
-    i = 0
-    while e > 1:
-        maps, optimal_paths, w = l.one_step(i)
-        for j, op in enumerate(optimal_paths[-1]):
-            cost += np.sum(original_costmap[np.asarray(op).T[:][0],
-                                            np.asarray(op).T[:][1]])
-            if i > 0:
-                cost_old += np.sum(original_costmap[np.asarray(old_op[j]).T[:][0],
-                                                    np.asarray(old_op[j]).T[:][1]])
-        # show(l.analysis, workspace, show_result)
-        old_op = optimal_paths[-1]
-        print(cost_old)
-        print(cost)
-        assert cost_old > cost
-        cost = 0
-        cost_old = 0
-        e = (np.absolute(w[-1] - w_old)).sum()
-        print("convergence: ", e)
-        w_old = copy.deepcopy(w[-1])
-        i += 1
-    show_multiple(maps, [original_costmap], workspace, show_result, directory=
-    home + '/../results/figures/maps_from_test_learch.png')
 
 
 if __name__ == "__main__":
--- tests/test_max_ent.py
+++ tests/test_max_ent.py
@@ -8,22 +8,37 @@ def test_maxEnt():
     nb_points = 40
     nb_rbfs = 5
     sigma = 0.1
-    nb_samples = 200
+    nb_samples = 500
+    nb_env = 1
 
     workspace = Workspace()
-    np.random.seed(1)
-    # Create random costmap
-    w, original_costmap, starts, targets, paths, centers = \
-        create_rand_env(nb_points, nb_rbfs, sigma, nb_samples,
-                        workspace)
+    m = MaxEnt(nb_points, nb_rbfs, sigma, workspace)
+    original_costmaps = []
+    original_starts = []
+    original_targets = []
+    original_paths = []
+    for i in range(nb_env):
+        np.random.seed(i)
+        # Create random costmap
+        w, original_costmap, starts, targets, paths, centers = \
+            load_environment("environment_sample_centers" + str(i))  # \
+        #    create_random_environment(nb_points, nb_rbfs, sigma, nb_samples, workspace)
+        original_costmaps.append(original_costmap)
+        starts = starts  # [:nb_samples]
+        targets = targets  # [:nb_samples]
+        paths = paths  # [:nb_samples]
+        original_paths.append(paths)
+        original_starts.append(starts)
+        original_targets.append(targets)
+        # Learn costmap
+        m.add_environment(centers, paths, starts, targets)
 
-    # Learn costmap
-    a = MaxEnt(nb_points, centers, sigma, paths, starts, targets, workspace)
-    maps, w_t = a.solve()
+    maps, optimal_paths, w_t, step = m.solve()
+
     print("weight difference: ", (np.absolute(w_t - w)).sum())
-    assert (np.absolute(w_t - w)).sum() < nb_points ** 2 * 0.4
+    assert (np.absolute(w_t - w)).sum() < nb_rbfs ** 2 * 0.6
     # Output learned costmaps
-    show_multiple(maps, [original_costmap], workspace, show_result,
+    show_multiple(maps, original_costmaps, workspace, show_result,
                   directory=home + '/../figures/maxEnt.png')
 
 
