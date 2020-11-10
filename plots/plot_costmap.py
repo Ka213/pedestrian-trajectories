45a029c0d8d81e178ec10f0db61aa5288f0ae06f
KatharinaH <katharina.hengel@gmail.com>
2020-11-10 13:55:42 +0100

    update plot scipts to new environment

--- plots/plot_costmap.py
+++ plots/plot_costmap.py
@@ -3,36 +3,44 @@ import common_import
 from my_utils.environment import *
 from my_utils.output_costmap import *
 from my_utils.my_utils import *
+from nn.random_environment_sdf import *
+from nn.dataset_sdf import *
 
 show_result = 'SHOW'
 with_trajectories = True
-average_cost = False
-nb_points = 40
-nb_rbfs = 5
-sigma = 0.1
-nb_samples = 100
+rbf = False
+nb_points = 28
+nb_rbfs = 4
+sigma = 0.15
+nb_samples = 5
 
 workspace = Workspace()
-np.random.seed(1)
+np.random.seed(0)
 
-w, costmap, starts, targets, paths, centers = create_rand_env(nb_points,
-                                                              nb_rbfs, sigma,
-                                                              nb_samples,
-                                                              workspace)
+if rbf:
+    w, costmap, starts, targets, paths, centers = \
+        create_env_rand_centers(nb_points, nb_rbfs, sigma, nb_samples, workspace)
+else:
+    workspaces = load_workspace_dataset(basename='sdf_data_100_10' + '.hdf5')
+    costmap = workspaces[4].costmap
+    starts = workspaces[4].starts[:nb_samples]
+    targets = workspaces[4].targets[:nb_samples]
+    paths = workspaces[4].demonstrations[:nb_samples]
 
-show_3D(costmap, workspace, show_result, starts=starts[:nb_samples],
-        targets=targets[:nb_samples], paths=paths[:nb_samples])
+# show_3D(costmap, workspace, show_result,
+#        starts=starts, targets=targets, paths=paths
+#        )
 
 # Output costmap
 if with_trajectories:
     title = 'costmap from {} differently weighted radial basis ' \
             'functions with sample trajectories'.format(nb_rbfs)
-    directory = home + '/../results/figures/costmap_{}rbf_with_samples.png'. \
+    directory = home + '/../results/figures/costmap_with_samples.pdf'. \
         format(nb_rbfs)
     show(costmap, workspace, show_result, directory=directory,
          starts=starts, targets=targets, paths=paths)
 else:
     title = 'costmap from {} differently weighted ' \
             'radial basis functions'.format(nb_rbfs)
-    directory = home + '../results/figures/costmap_{}rbf.png'.format(nb_rbfs)
+    directory = home + '../results/figures/costmap_{}rbf.pdf'.format(nb_rbfs)
     show(costmap, workspace, show_result, directory=directory, title=title)
--- plots/plot_training_map.py
+++ plots/plot_training_map.py
@@ -1,60 +1,69 @@
 import common_import
 
-from my_utils.environment import *
+from nn.dataset_sdf import *
 from my_utils.output_costmap import *
 from my_utils.my_utils import *
 from nn.nn_utils import *
 
 show_result = 'SHOW'
-algorithm = 'loss_aug_occ'
-nb_points = 28
-nb_rbfs = 4
-sigma = 0.1
-nb_samples = 1
+algorithm = 'loss_aug_esf'
+nb_samples = 10
 
-loss_stddev = 3
+loss_stddev = 10
 loss_scalar = 1
 N = 35
 
-workspace = Workspace()
-np.random.seed(1)
+np.random.seed(0)
 
-w, original_costmap, starts, targets, paths, centers = \
-    create_env_rand_centers(nb_points, nb_rbfs, sigma, nb_samples,
-                            Workspace())
-phi = get_phi(nb_points, centers, sigma, Workspace())
+workspaces = load_workspace_dataset(basename='sdf_data_100_' +
+                                             str(nb_samples) + '.hdf5')
+costs = workspaces[0].costmap
+starts = workspaces[0].starts
+targets = workspaces[0].targets
+demonstrations = workspaces[0].demonstrations
+occupancy = workspaces[0].occupancy
 
-map = np.zeros((nb_points, nb_points))
+box = EnvBox()
+lims = workspaces[0].lims
+box.dim[0] = lims[0][1] - lims[0][0]
+box.dim[1] = lims[1][1] - lims[1][0]
+box.origin[0] = box.dim[0] / 2.
+box.origin[1] = box.dim[1] / 2.
+workspace_box = Workspace(box)
+
+map = np.ones(costs.shape)
 if algorithm == 'learch':
-    map = get_costmap(phi, np.exp(np.ones((nb_rbfs ** 2))))
-    map, op = get_learch_target(map, paths, starts, targets, loss_stddev,
-                                loss_scalar)
+    map, op = get_learch_target(map, demonstrations, starts,
+                                targets, loss_stddev, loss_scalar,
+                                workspace_box)
 elif algorithm == 'maxEnt':
-    map = get_maxEnt_target(map, N, paths, starts, targets, phi)
+    map, f_expected, f_empirical = get_maxEnt_target(map, N, demonstrations, starts,
+                                                     targets, workspace_box)
 elif algorithm == 'occ':
-    map = get_esf_target(map, N, paths, starts, targets)
-elif algorithm == 'loss_aug_occ':
-    map = get_loss_aug_est_target(map, paths, starts, targets, loss_scalar,
-                                  loss_stddev, N)
-elif algorithm == 'occ_learch':
+    map = get_esf_target(map, N, demonstrations, starts, targets,
+                         workspace_box)
+elif algorithm == 'loss_aug_esf':
+    map = get_loss_aug_esf_target(map, demonstrations, starts,
+                                  targets, loss_scalar, loss_stddev,
+                                  N, workspace_box)
+elif algorithm == 'avg_learch_esf':
     map = get_avg_learch_esf_target()
-elif algorithm == 'test':
-    map = original_costmap
-elif algorithm == 'test_exp':
-    map = get_costmap(phi, np.exp(w))
 
-occupancy = original_costmap < 0.6
-
-one_map = get_costmap(phi, np.ones(nb_rbfs ** 2))
-
+# one_map = get_costmap(phi, np.ones(nb_rbfs ** 2))
+print(occupancy.shape)
+print(map.shape)
+print(costs.shape)
 # Output costmap
-# startmap = get_costmap(phi, np.exp(np.ones(nb_rbfs ** 2)))
-# _,_, op = plan_paths(nb_samples, startmap, workspace, starts, targets)
-
-
-show_multiple([map], [original_costmap], workspace, show_result
-              # ,starts=starts, targets=targets, paths=paths, optimal_paths=[op]
-              )
-show(map, workspace, show_result
+# show_multiple([occupancy, map], [costs], workspace_box, show_result
+# ,starts=starts, targets=targets, paths=paths#, optimal_paths=[op]* 2
+#              )
+# show(occupancy, workspace_box, show_result,
+#     # ,starts=starts, targets=targets, paths=paths, optimal_paths=[op]
+#     directory = home + '/../results/figures/occupancy.pdf')
+show(map, workspace_box, show_result,
      # ,starts=starts, targets=targets, paths=paths, optimal_paths=[op]
+     directory=home + '/../results/figures/nn_target_loss_aug_esf_map.pdf')
+show(costs, workspace_box, show_result
+     , starts=starts, targets=targets, paths=demonstrations,  # optimal_paths=op,
+     directory=home + '/../results/figures/nn_costs_loss_aug_esf.pdf'
      )
